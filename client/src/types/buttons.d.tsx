c5e900a38c34ebaf4ce65efe6bbbfb984b507d55
Daniel Almanza <93274949+devDeam@users.noreply.github.com>
2025-05-02 12:14:58 -0500

    Revert "Front"

--- client/src/types/buttons.d.tsx
+++
@@ -1,9 +0,0 @@
-export interface BackButtonProps {
-    label: string;
-    style?: React.CSSProperties;
-  }
-
-export interface FormButtonsProps {
-    onClick: () => void;
-    style?: React.CSSProperties;
-  }
