897bf9bfd8f41339b5359e58592a3e994b0902c6
devDeam <daniel.deam123@gmail.com>
2025-05-05 01:24:44 -0500

    Se agrega test para probar endpoint api/create

--- server/server.js
+++ server/server.js
@@ -22,5 +22,14 @@ app.get("/", (req, res) => {
   res.send("BIENVENIDO A LA API DE INFX");
 });
 
+// Puerto dinamico
+const PORT = process.env.PORT || 3000;
+// Conexión a la base de datos y arranque del servidor
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+});
+
 // Se exporta solo 'app' para que sea usada en los tests sin iniciar el servidor
 module.exports = { app };
