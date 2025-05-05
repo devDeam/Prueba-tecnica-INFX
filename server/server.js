d76e9b27f8abd81190b0f0ad0397d243ba46de4e
devDeam <daniel.deam123@gmail.com>
2025-05-05 01:03:02 -0500

    Sea agrega test unitario al BACK endpoint POST api/create

--- server/server.js
+++ server/server.js
@@ -22,11 +22,5 @@ app.get("/", (req, res) => {
   res.send("BIENVENIDO A LA API DE INFX");
 });
 
-// Puerto dinamico
-const PORT = process.env.PORT || 3000;
-// Conexión a la base de datos y arranque del servidor
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-  });
-});
+// Se exporta solo 'app' para que sea usada en los tests sin iniciar el servidor
+module.exports = { app };
